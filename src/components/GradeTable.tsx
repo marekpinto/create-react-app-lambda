255ce4e56aac0e4780dd5331f3f5ce85531e1f8d
Marek Pinto <marek.pinto@gmail.com>
2024-01-21 18:15:30 -0500

    Implemented DataGrid for Table

--- src/components/GradeTable.tsx
+++ src/components/GradeTable.tsx
@@ -1,3 +1,7 @@
+import Box from '@mui/material/Box';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+
+
 /**
  * You might find it useful to have some dummy data for your own testing.
  * Feel free to write this function if you find that feature desirable.
@@ -5,9 +9,53 @@
  * When you come to office hours for help, we will ask you if you have written
  * this function and tested your project using it.
  */
+
+//Citation: This code based on example code at https://mui.com/x/react-data-grid/
+
 export function dummyData() {
-  return [];
+  return [
+    { id: "U125", name:"Hirluin", classId: 'C125', className: 'ST 519', semester: 'fall2022', finalGrade: 395.8 },
+    { id: "U127", name:"Marek", classId: 'C125', className: 'ST 519', semester: 'fall2022', finalGrade: 128.7 },
+    { id: "U130", name:"Bard", classId: 'C125', className: 'ST 519', semester: 'fall2022', finalGrade: 458.3 },
+    { id: "U131", name:"Charlie", classId: 'C125', className: 'ST 519', semester: 'fall2022', finalGrade: 523.4 },
+    { id: "U132", name:"David", classId: 'C125', className: 'ST 519', semester: 'fall2022', finalGrade: 633.3 },
+    { id: "U133", name:"Fargo", classId: 'C125', className: 'ST 519', semester: 'fall2022', finalGrade: 553.2 },
+    { id: "U134", name:"Joyce", classId: 'C125', className: 'ST 519', semester: 'fall2022', finalGrade: 400 },
+    { id: "U135", name:"Brown", classId: 'C125', className: 'ST 519', semester: 'fall2022', finalGrade: 458.2 },
+    { id: "U136", name:"Jimmy", classId: 'C125', className: 'ST 519', semester: 'fall2022', finalGrade: 390.2 },
+  ];
 }
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'Student ID', width: 90 },
+  {
+    field: 'name',
+    headerName: 'Student Name',
+    width: 150,
+  },
+  {
+    field: 'classId',
+    headerName: 'Class ID',
+    width: 100,
+  },
+  {
+    field: 'className',
+    headerName: 'Class Name',
+    width: 120,
+  },
+  {
+    field: 'semester',
+    headerName: 'Semester',
+    width: 130,
+  },
+  {
+    field: 'finalGrade',
+    headerName: 'Final Grade',
+    width: 90,
+    type: 'number',
+
+  },
+];
 
 /**
  * This is the component where you should write the code for displaying the
@@ -17,5 +65,23 @@ export function dummyData() {
  *
  */
 export const GradeTable = () => {
-  return <></>;
+  return (
+    <Box sx={{ height: 400, width: '90%' }}>
+      <DataGrid
+        rows={dummyData()}
+        columns={columns}
+        pagination
+        initialState={{
+          pagination: {
+            paginationModel: {
+              page: 0, pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5,10,20,50,100]}
+
+      />
+    </Box>
+  );
 };
+
