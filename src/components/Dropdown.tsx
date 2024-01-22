7282021d53d4800eab666a5ee682147dbbe4c502
Marek Pinto <marek.pinto@gmail.com>
2024-01-21 19:23:38 -0500

    Finished API Calls to populate Dropdown.

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -3,20 +3,24 @@ import MenuItem from '@mui/material/MenuItem';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 import {BASE_API_URL, GET_DEFAULT_HEADERS, MY_BU_ID} from "../globals";
+import { IUniversityClass } from "../types/api_types";
+
 
 
 // This component was inspired by code from GPT-3.5
 export const Dropdown = () => {
-  const [classes, setClasses] = useState<string[]>([]);
+  const [classes, setClasses] = useState<IUniversityClass[]>([]);
   const [selectedClass, setSelectedClass] = useState<string>('');
 
   useEffect(() => {
     const fetchData = async () => {
         const headers = GET_DEFAULT_HEADERS();
-        const res = await fetch(BASE_API_URL + "/class/listBySemester/fall2022?buid=" + , {
+        const res = await fetch(BASE_API_URL + "/class/listBySemester/fall2022?buid=" + MY_BU_ID, {
           method: "GET",
+          headers: headers,
         });
-        const json = await res.json();
+        const json: IUniversityClass[] = await res.json();
+        console.log(json);
         setClasses(json);
       };
 
@@ -24,14 +28,14 @@ export const Dropdown = () => {
   }, []); // Run this effect only once when the component mounts
 
   const handleChange = (event: SelectChangeEvent<string>) => {
-    setSelectedClass(event.target.value as string);
+    setSelectedClass((event.target.value));
   };
 
   return (
     <Select value={selectedClass} onChange={handleChange} fullWidth={true} label="Class">
-      {classes.map((course: string) => (
-        <MenuItem key={course} value={course}>
-          {course}
+      {classes.map((course) => (
+        <MenuItem key={course.classId} value={course.classId}>
+          {course.title}
         </MenuItem>
       ))}
     </Select>
